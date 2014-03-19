eedec04380cac2234b79ce48c6848e83fb70bbda
nihark <nihar@Nihars-MacBook-Air.local>
2014-03-19 11:03:36 +0530

    Added support for showing bar values, positioning bar labels and updated examples accordingly

--- js/espn.gfx.stacked.js
+++ js/espn.gfx.stacked.js
@@ -1,7 +1,9 @@
+"use strict";
+
 d3.chart("stacked", {
 
 	config : {
-		space : 2,
+		space : 1,
 		barSpacing : 27,
 		colors : ["#aad", "#8080a2", "#556"],
 		strokeColor : "rgb(6,120,155)",
@@ -9,16 +11,18 @@ d3.chart("stacked", {
 		titleHeight : 13,
 		keySquareSize : 13,
 		keySpacing : 55,
-		keyRightMargin : 16,
-		keyBottomMargin : 11,
+		keyLeftMargin : 0,
+		keyTextRightMargin : 16,
+		keyTextBottomMargin : 11,
 		keyClass : 'key',
-		keyYPos : 0,
 		leftStackClass : 'left',
 		rightStackClass : 'right',
 		titleAttr : {"font-weight":"bold","font-size":"1.2em"},
 		labelAttr : {"font-size":"1em"},
+		valueAttr : {"font-size":"1.1em"},
 		titleClass : 'title',
-		labelClass : 'label'
+		labelClass : 'label',
+		valueClass : 'value'
 	},
 
 	initialize: function(options) {
@@ -31,9 +35,13 @@ d3.chart("stacked", {
 			hideLine = options.hideLine,
 			hideText = options.hideText,
 			hideLabel = options.hideLabel,
+			showValue = options.showValue,
+			keyLeftMargin = options.keyLeftMargin || chart.config.keyLeftMargin,
 			strokeColor = options.strokeColor || this.config.strokeColor,
 			titleAttr = options.titleAttr || this.config.titleAttr,
 			labelAttr = options.labelAttr || this.config.labelAttr,
+			valueAttr = options.valueAttr || this.config.valueAttr,
+			labelPos = options.labelPos,
 			space = options.space || this.config.space;
 
 		this.topMargin = options.topMargin || this.config.topMargin;
@@ -85,15 +93,14 @@ d3.chart("stacked", {
 				.attr('style',function(d,i){
 					return 'fill:' + chart.color(d.layer);
 				})
-				.attr("height", function(){
-					var height = (barHeight || chart.y.rangeBand() - barSpacing);
-					return (height > 0) ? height : 1;
-				})
+				.attr("height", getBarHeight)
 				.attr("width", function(d) { return chart.x(d.y)/2; });
 
-				// TODO: Move to a new layer? & Improve solution
-				if(!hideLine || !hideText || !hideLabel){
+				// TODO: Move to a new layer? & Improve solution for adding/updating text
+				if(!hideLine || !hideText || !hideLabel || showValue){
+
 					this.each(function(d,i){
+
 						var parent = d3.select(this.parentNode),
 							height,
 							text,
@@ -105,9 +112,8 @@ d3.chart("stacked", {
 								.attr('dx',chart.width()/2)
 								.attr('dy',chart.y(d.x))
 								.attr(titleAttr)
-								.text(function(){
-									return d.title;
-								});
+								.text(d.title);
+
 							bbox = text[0][0].getBBox();
 							//place text element in DOM, calculate dimensions and reassign dx dy to center text
 							text.attr('dx',chart.width()/2 - bbox.width/2)
@@ -121,27 +127,48 @@ d3.chart("stacked", {
 								.attr('text-anchor','end')
 								.attr('dy',chart.y(d.x))
 								.attr(labelAttr)
-								.text(function(){
-									return d.label;
-								});
+								.text(d.label);
+
 							bbox = text[0][0].getBBox();
-							text.attr('dx',chart.width())
-							text.attr('dy',chart.y(d.x) - bbox.height/2)
+
+							text.attr('dx',chart.width());
+
+							/* TODO: Make more flexible. Right now, only used in preferred shot graph */
+							if(labelPos === 'bottom'){
+								text.attr('dy',chart.y(d.x) + getBarHeight() + bbox.height)
+									.attr('dx',chart.width()/2 + bbox.width + (space * 4));
+							}else{
+								text.attr('dy',chart.y(d.x) - bbox.height/2);
+							}
 						}
 
 						height = (bbox && bbox.height);
+
 						height = height ? height: chart.config.titleHeight;
-						barHeight  = (barHeight || chart.y.rangeBand() - barSpacing);
 
 						if(!hideLine){
 							parent.append('line')
 								.style("stroke", strokeColor)
-								.attr('x1',chart.width()/2)
-								.attr('y1',chart.y(d.x) - height)
-								.attr('x2',chart.width()/2)
-								.attr('y2',function(){
-									return (i==0) ? 0 : chart.y(d.x) - (chart.y(1) - chart.y(0) - barHeight);
+								.attr('x1', chart.width()/2)
+								.attr('y1', chart.y(d.x) - height)
+								.attr('x2', chart.width()/2)
+								.attr('y2', function(){
+									return (i==0) ? 0 : chart.y(d.x) - (chart.y(1) - chart.y(0) - getBarHeight());
 								});
+						}
+
+						if(showValue){
+							text = parent.append('text')
+								.attr('class', chart.config.valueClass)
+								.attr('dx', chart.x(d.y)/4 + (chart.x(d.y0)/2 + chart.width()/2) + space/2)
+								.attr(valueAttr)
+								.text(function(){
+									return (d.y > 0) ? d.y : '';
+								});
+
+							bbox = text[0][0].getBBox();
+
+							text.attr('dy', chart.y(d.x) + (getBarHeight()/2) + (bbox.height/4));
 						}
 					});
 				}
@@ -150,31 +177,37 @@ d3.chart("stacked", {
 		function onTrans(){
 			this.attr("y", function(d) { return chart.y(d.x); })
 				.attr("x", function(d) { return (chart.x(d.y0)/2 + chart.width()/2) + space/2; })
-				.attr("height", function(){
-					var height = (barHeight || chart.y.rangeBand() - barSpacing);
-					return (height > 0) ? height : 1;
-				})
+				.attr("height", getBarHeight)
 				.attr("width", function(d) { return chart.x(d.y)/2; });
 
-			if(!hideText || !hideLabel){
+			if(!hideText || !hideLabel || showValue){
 
 				this.each(function(d,i){
-					var parent = d3.select(this.parentNode);
+					var parent = d3.select(this.parentNode),
+						text,
+						bbox;
 
 					if(!hideLabel && d.label){
 						parent.select('.' + chart.config.labelClass)
-						.text(function(){
-							return d.label;
-						});
+						.text(d.label);
 					}
 
 					if(!hideText && d.title){
 						parent.select('.' + chart.config.titleClass)
-						.text(function(){
-							return d.title;
-						});
+						.text(d.title);
 					}
 
+					if(showValue){
+						text = parent.select('.' + chart.config.valueClass)
+						.attr('dx', (chart.x(d.y)/4) + ((chart.x(d.y0)/2 + chart.width()/2) + space/2))
+						.text(function(){
+							return (d.y > 0) ? d.y : '';
+						});
+
+						bbox = text[0][0].getBBox();
+						
+						text.attr('dy', chart.y(d.x) + (getBarHeight()/2) + (bbox.height/4));
+					}
 				});
 			}
 		}
@@ -182,16 +215,6 @@ d3.chart("stacked", {
 		function onExit(){
 			/* TODO: Remove nodes */
 		}
-
-		this.layer("rightAxis").on("merge", function(){
-			this.selectAll('rect')
-				.attr("y", function(d) { return chart.y(d.x); })
-				.attr("x", function(d) { return (chart.x(d.y0)/2 + chart.width()/2) + space/2; })
-				.attr("height", function(){
-					return (barHeight || chart.y.rangeBand() - barSpacing);
-				})
-				.attr("width", function(d) { return chart.x(d.y)/2; });
-		});
 
 		this.layer("rightAxis").on("exit", onExit);
 		this.layer("rightAxis").on("enter", onEnter);
@@ -218,27 +241,47 @@ d3.chart("stacked", {
 				.attr('style',function(d,i){
 					return 'fill:' + chart.color(d.layer);
 				})
-				.attr("height", function(){
-					var height = (barHeight || chart.y.rangeBand() - barSpacing);
-					return (height > 0) ? height : 1;
-				})
+				.attr("height", getBarHeight)
 				.attr("width", function(d) { return chart.x(d.y)/2; });
 
-				if(!hideLabel){
+				if(!hideLabel || showValue){
+
 					this.each(function(d,i){
-							if(d.label){
-								var parent = d3.select(this.parentNode),
+						
+						var parent = d3.select(this.parentNode),
+							text,
+							bbox;
+
+							if(!hideLabel && d.label){
 								text = parent.append('text')
 									.attr('dx',0)
 									.attr('class',chart.config.labelClass)
 									.attr('text-anchor','start')
 									.attr('dy',chart.y(d.x))
 									.attr(labelAttr)
-									.text(function(){
-										return d.label;
-									});
+									.text(d.label);
+
 								bbox = text[0][0].getBBox();
-								text.attr('dy',chart.y(d.x) - bbox.height/2)
+
+								if(labelPos === 'bottom'){
+									text.attr('dy',chart.y(d.x) + getBarHeight() + bbox.height)
+										.attr('dx',chart.width()/2 - bbox.width - (space * 4));
+								}else{
+									text.attr('dy',chart.y(d.x) - bbox.height/2);
+								}
+							}
+
+							if(showValue){
+								text = parent.append('text')
+									.attr('class',chart.config.valueClass)
+									.attr('dx', (chart.x(d.y)/4) + ((chart.width() - chart.x(d.y) - chart.x(d.y0))/2 - space/2))
+									.attr(valueAttr)
+									.text(function(){
+										return (d.y > 0) ? d.y : '';
+									});
+
+								bbox = text[0][0].getBBox();
+								text.attr('dy', chart.y(d.x) + (getBarHeight()/2) + (bbox.height/4));
 							}
 					});
 				}
@@ -248,23 +291,40 @@ d3.chart("stacked", {
 		function onTransLeft(){
 			this.attr("y", function(d) { return chart.y(d.x); })
 				.attr("x", function(d) { return (chart.width() - chart.x(d.y) - chart.x(d.y0))/2 - space/2; })
-				.attr("height", function(){
-					var height = (barHeight || chart.y.rangeBand() - barSpacing);
-					return (height > 0) ? height : 1;
-				})
+				.attr("height", getBarHeight)
 				.attr("width", function(d) { return chart.x(d.y)/2; });
 
-			if(!hideLabel){
+			if(!hideLabel || showValue){
+
 				this.each(function(d,i){
+
+					var parent = d3.select(this.parentNode),
+						text,
+						bbox;
+
 					if(d.label){
-						d3.select(this.parentNode)
-						.select('.' + chart.config.labelClass)
+						parent.select('.' + chart.config.labelClass)
+						.text(d.label);
+					}
+
+					if(showValue){
+						text = parent.select('.' + chart.config.valueClass)
+						.attr('dx', chart.x(d.y)/4 + (chart.width() - chart.x(d.y) - chart.x(d.y0))/2 - space/2)
 						.text(function(){
-							return d.label;
+							return (d.y > 0) ? d.y : '';
 						});
+
+						bbox = text[0][0].getBBox();
+
+						text.attr('dy', chart.y(d.x) + (getBarHeight()/2) + (bbox.height/4));
 					}
 				});
 			}
+		}
+
+		function getBarHeight(){
+			var height = (barHeight || chart.y.rangeBand() - barSpacing);
+			return (height > 0) ? height : 1;
 		}
 
 		function onExitLeft(){
@@ -293,7 +353,7 @@ d3.chart("stacked", {
 				events: {
 					enter: function() {
 						this.attr("x", function(d,i){
-							return (chart.config.keySpacing * i);
+							return (chart.config.keySpacing * i) + (keyLeftMargin);
 						})
 						.attr("y", chart.config.keyYPos)
 						.attr("height", chart.config.keySquareSize)
@@ -301,13 +361,12 @@ d3.chart("stacked", {
 						.style("fill", function(d,i){
 							return chart.color(i);
 						}).each(function(d,i){
-							// Setting values through config, as centering text, requires to calculate its dimensions
 							chart.base.select('.' + chart.config.keyClass).append('text')
 								.attr('dx',function(){
-									return (chart.config.keySpacing * i) + (chart.config.keyRightMargin);
+									return (chart.config.keySpacing * i) + (keyLeftMargin + chart.config.keyTextRightMargin);
 								})
 								.attr('dy',function(){
-									return chart.config.keyBottomMargin;
+									return chart.config.keyTextBottomMargin;
 								})
 								.text(function(){
 									return d;
@@ -340,10 +399,12 @@ d3.chart("stacked", {
 
 	transform: function(dataSrc) {
 
-		/* TODO: type checks */
+		/* TODO: data checks */
 		var stack = d3.layout.stack(),
 			leftStack = dataSrc[0],
-			rightStack = dataSrc[1];
+			rightStack = dataSrc[1],
+			y0StackMax,
+			y1StackMax;
 
 		this.length = leftStack[0].length; //assuming both data sets are equal in length
 
@@ -351,16 +412,16 @@ d3.chart("stacked", {
 
 		rightStack = stack(rightStack);
 
-		//the largest single layer
-		this.yGroupMax = d3.max(leftStack, function(layer) { return d3.max(layer, function(d) { return d.y; }); }),
-		//the largest stack
-		this.yStackMax = d3.max(leftStack, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); });            
+		// get the largest layer from each stack and use the higher value for x domain
+		y0StackMax = d3.max(leftStack, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); });
+
+		y1StackMax = d3.max(rightStack, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); });
+
+		this.x.domain([0, Math.max(y0StackMax,y1StackMax)])
+			.range([0, this.width()]);
 
 		this.y.domain(d3.range(this.length))
-			.rangeRoundBands([this.topMargin, this.height()], .08);
-
-		this.x.domain([0, this.yStackMax])
-			.range([0, this.width()]);
+			.rangeBands([this.topMargin, this.height() + this.topMargin/2]);
 
 		this.color.domain(d3.range(dataSrc[0].length))
 			.range(this.colors);
