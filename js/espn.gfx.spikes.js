46a480d4da363cc29076377afd15b733f765c981
nihark <nihar@Nihars-MacBook-Air.local>
2014-03-24 08:31:28 +0530

    Cleaned up spikes wagonwheel

--- js/espn.gfx.spikes.js
+++ js/espn.gfx.spikes.js
@@ -50,7 +50,10 @@ d3.chart('spikes', {
 			gradientColors = options.gradientColors || chart.config.gradientColors,
 			shade = options.shade,
 			offset = options.offset || chart.config.offset,
-			colorMap = options.colorMap || chart.config.colorMap;
+			colorMap = options.colorMap || chart.config.colorMap,
+			wrapper,
+			center,
+			gradient;
 
 		this.base = this.base.append('svg');
 
@@ -58,11 +61,11 @@ d3.chart('spikes', {
 
 		this.height(diameter);
 
-		this.center = diameter / 2;
+		center = diameter / 2;
 
-		this.ground = this.base.append('circle')
-			.attr('cx', this.center)
-			.attr('cy', this.center)
+		this.base.append('circle')
+			.attr('cx', center)
+			.attr('cy', center)
 			.attr('r', radius)
 			.attr('stroke-width', '5')
 			.attr('stroke', chart.config.ground.stroke)
@@ -70,9 +73,9 @@ d3.chart('spikes', {
 
 		// Append Pitch
 		this.base.append('rect')
-			.attr('x', this.center - chart.config.pitch.width / 2)
+			.attr('x', center - chart.config.pitch.width / 2)
 			.attr('rx', chart.config.pitch.radius)
-			.attr('y', this.center - chart.config.pitch.height / 2)
+			.attr('y', center - chart.config.pitch.height / 2)
 			.attr('ry', chart.config.pitch.radius)
 			.attr('width', chart.config.pitch.width)
 			.attr('height', chart.config.pitch.height)
@@ -80,29 +83,29 @@ d3.chart('spikes', {
 
 		// Append 30 yards circle
 		this.base.append('rect')
-			.attr('x', this.center - chart.config.ring.width / 2)
+			.attr('x', center - chart.config.ring.width / 2)
 			.attr('rx', chart.config.ring.radius)
-			.attr('y', this.center - chart.config.ring.height / 2)
+			.attr('y', center - chart.config.ring.height / 2)
 			.attr('ry', chart.config.ring.radius)
 			.attr('width', chart.config.ring.width)
 			.attr('height', chart.config.ring.height)
 			.attr('opacity', chart.config.ring.opacity)
 			.attr('fill', chart.config.ring.fill);
 
-		this.wrapper = this.base.append('g');
+		wrapper = this.base.append('g');
 
 		// Gradient
-		this.gradient = this.base.append('defs')
+		gradient = this.base.append('defs')
 			.append('radialGradient')
 			.attr('id', chart.config.gradientDefId)
 			.attr('r', '65%');
 
-		this.gradient.append('stop')
+		gradient.append('stop')
 			.attr('offset', '0%')
 			.attr('stop-color', chart.config.gradientColors[0])
 			.attr('stop-opacity', '1');
 
-		this.gradient.append('stop')
+		gradient.append('stop')
 			.attr('offset', '100%')
 			.attr('stop-color', chart.config.gradientColors[1])
 			.attr('stop-opacity', '1');
@@ -118,7 +121,7 @@ d3.chart('spikes', {
 		function onEnter() {
 
 			this.attr('d', function(d, i) {
-				return 'M' + chart.center + ',' + (chart.center - offset) + 'L' + d.x + ',' + d.y;
+				return 'M' + center + ',' + (center - offset) + 'L' + d.x + ',' + d.y;
 			})
 				.attr('stroke', function(d, i) {
 					return colorMap[d.runs] || '#fff';
@@ -133,7 +136,7 @@ d3.chart('spikes', {
 		function onTrans() {
 
 			this.attr('d', function(d, i) {
-				return 'M' + chart.center + ',' + (chart.center - offset) + 'L' + d.x + ',' + d.y;
+				return 'M' + center + ',' + (center - offset) + 'L' + d.x + ',' + d.y;
 			})
 				.attr('stroke', function(d, i) {
 					return colorMap[d.runs] || '#fff';
@@ -141,7 +144,7 @@ d3.chart('spikes', {
 		}
 
 		function dataBind(data) {
-			return chart.wrapper.selectAll('.' + chart.config.spikesClass)
+			return wrapper.selectAll('.' + chart.config.spikesClass)
 				.data(data);
 		}
 
@@ -149,7 +152,7 @@ d3.chart('spikes', {
 			return this.append('path').attr('class', chart.config.spikesClass);
 		}
 
-		var spikes = this.layer('spikes', this.wrapper, {
+		var spikes = this.layer('spikes', wrapper, {
 			dataBind: dataBind,
 			insert: insert
 		});
